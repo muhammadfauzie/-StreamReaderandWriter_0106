45b1e26e93d107b437f99a51f347872067cddb57
fauzie primarka <fauzieprimarka@gmail.com>
2025-06-10 17:44:29 +0700

    menambahkan penutupan file setelah menulis dan membuka file untuk membaca

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -20,4 +20,11 @@ int main() {
         //menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
-   
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+    //Membuka file dalam mode membaca.
+    ifstream infile;
+    //menunjuk ke sebuah nama file.
+    infile.open("contohfile.txt");
+    cout << endl << ">= Membuka dan Membaca file" << endl;
+    
