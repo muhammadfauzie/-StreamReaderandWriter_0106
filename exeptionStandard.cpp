311f7c4d0693747091842c1d6b1f779ab7c43142
fauzie primarka <fauzieprimarka@gmail.com>
2025-06-10 18:48:59 +0700

    Menambahkan fungsionalitas untuk membuka, menulis, dan membaca file, serta menangani kesalahan jika file tidak ditemukan.

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -4,3 +4,24 @@
 #include <array>
 //untuk obyek array yang akan kita digunakan
 using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = {10, 20, 30};
+        //pesan array integer 3 elemen
+        cout<<data.at(5)<<endl;
+        //memanggil array elemen ke 5
+    }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan di eksekusi karna array data hanya memiliki 3 elemen*/
+       
+    }
+    cout << "Baris program terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti 
+    meskipun terjadi kesalahan*/
+    return 0; 
+}
