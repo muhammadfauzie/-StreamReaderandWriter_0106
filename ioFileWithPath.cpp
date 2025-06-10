eda80b3754985fd15df80a7b5cdb398a9d0405ec
fauzie primarka <fauzieprimarka@gmail.com>
2025-06-10 18:19:16 +0700

    loop akan berhenti jika anda memasukkan karakter q

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -18,4 +18,6 @@ int main(){
         cout << "- ";
         // mendapatkan setiap karakter dalam satu baris 
         getline(cin, baris);
-       
+        // loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+    }    
